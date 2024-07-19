625b438e8761965c53d2bf4a211fd5d1b3e47c8a
Mel Angelo Mendez <mel.mendez37@gmail.com>
2024-07-20 02:23:07 +0800

    fixed reference error in next-theme-provider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+import { NextThemeProvider } from "@/lib/NextThemeProvider";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <NextThemeProvider>
-      {children}
+        {children}
       </NextThemeProvider>
       </body>
     </html>
