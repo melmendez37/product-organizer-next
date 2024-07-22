598c308ab72e00619f8ac48560a383239aa2682e
Mel Angelo Mendez <mel.mendez37@gmail.com>
2024-07-22 22:24:10 +0800

    authentication by clerk is now working

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,10 +1,31 @@
+import { auth } from "@clerk/nextjs/server";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const HomePage = () => {
-  return(
-    <div>
-      Hello
-    </div>
+const HomePage = async () => {
+  const { userId } = auth();
+  if (userId) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <section className="py-6 ">
+      <div className="text-center w-full mx-auto py-12 px-4 sm:px-4 lg:py-16 lg:px-8">
+        <h1 className="text-3xl font-extrabold text-black sm:text-2xl">
+          <span className="block">Personal Item Organizer</span>
+          <span className="text-purple-300">Your Personal Item Organizer</span>
+        </h1>
+        <div className="lg:mt-0 lg:flex-shrink-0">
+          <div className="mt-12 inline-flex rounded-md">
+            <Link href={"/sign-in"}>
+              <Button className="bg-purple-800">Sign In Here</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default HomePage;
