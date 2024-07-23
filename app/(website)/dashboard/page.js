578dd3c61d6af6ff4ab409092ef83563369eadc8
Mel Angelo Mendez <mel.mendez37@gmail.com>
2024-07-23 17:54:46 +0800

    implemented the ADD and DELETE functionality of categories in dashboard

--- app/(website)/dashboard/page.js
+++ app/(website)/dashboard/page.js
@@ -1,6 +1,10 @@
+import { addCategory } from "@/lib/actions/add-category";
+import { deleteCategory } from "@/lib/actions/delete-category";
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
+
 
 const Dashboard = async () => {
   const user = await currentUser();
@@ -13,7 +17,22 @@ const Dashboard = async () => {
 
   return (
     <section className="p-24 space-y-6 min-h-screen flex flex-col ">
-      <div>Add category</div>
+      <div>
+        <form 
+          className="flex" 
+          action={addCategory}
+        >
+          <input hidden 
+            id="creator" 
+            name="creator" 
+            defaultValue={creator}/>
+          <Input name="name" id="name" placeholder="Name"/>
+          <Button type="submit">
+            Add category
+          </Button>
+        </form>
+      </div>
+
       {categories.length > 0 ? (
         <>
           <table className="min-w-full leading-normal text-center">
@@ -37,15 +56,24 @@ const Dashboard = async () => {
               {categories.map((category) => (
                 <tr key={category.id}>
                   <td className="px-5 py-3 text-sm font-normal text-center text-grey-800 border-b border-grey-200">
-                    <Link href={`/dashboard/category/${category.id}`}>
+                    <Link
+                      
+                      href={`/dashboard/category/${category.id}/update`}>
                       {category.name}
                     </Link>
                   </td>
-                  <td className="px-5 py-3 text-sm font-normal text-center text-grey-800 border-b border-grey-200">
-                    <Link href={`/dashboard/category/${category.id}/update`}>
+                  <td className="px-5 py-3 text-sm font-normal text-center flex justify-center items-center text-grey-800 border-b border-grey-200">
+                    <Link
+                    className="mr-1"
+                    href={`/dashboard/category/${category.id}/update`}>
                       Details
                     </Link>
-                    Delete
+                    <form action={deleteCategory}>
+                      <input hidden defaultValue={category.id} name="id" />
+                      <Button variant="light" type="submit">
+                        Delete
+                      </Button>
+                    </form>
                   </td>
                 </tr>
               ))}
